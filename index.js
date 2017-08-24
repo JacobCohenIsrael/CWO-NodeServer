3c5ef5f9a33f3a2f469dc2db8d823ec7d2d6a7a6
Cohen, Jacob <Jacob.Cohen@scientificgames.com>
2017-08-24 11:48:58 +0300

    * Connected some nodes and added jump distance logic. * Added notifications

--- index.js
+++ index.js
@@ -6,72 +6,73 @@ require('./config/app.configurations')(app);
 const playersDb = require('./tempDB/playerDb.json');
 const nodeDb = require('./tempDB/nodeDb.json');
 const players = {};
+const connectionsId = {};
 const nodes = {};
 const nodesCoords = {};
 let idCounter = 2;
 const newPlayer = {
-    id : idCounter,
-	firstName : "Smith",
-	currentNodeName : "Earth",
-	isLanded : true,
-	homePlanetName : "Earth",
-	credits : 10,
-	activeShipIndex : 0,
-	token : "3adasd2ds-63af-408b-9ce2-931631c0bbed",
-	ships : [
-		{
-			id : 1,
-			currentHullAmount: 1,
-			currentShieldAmount : 1,
-            currentEnergyAmount : 1,
+    id: idCounter,
+    firstName: "Smith",
+    currentNodeName: "Earth",
+    isLanded: true,
+    homePlanetName: "Earth",
+    credits: 10,
+    activeShipIndex: 0,
+    token: "3adasd2ds-63af-408b-9ce2-931631c0bbed",
+    ships: [
+        {
+            id: 1,
+            currentHullAmount: 1,
+            currentShieldAmount: 1,
+            currentEnergyAmount: 1,
             shipClass: "jumper",
             shipType: "Ancients",
-			shipCargo : {},
-			shipParts : [
-				{
-					"name": "BasicEngine",
-					"partStats" : {
-						"hull": 50,
-						"jumpDistance" : 10
-					}
-				},
-				{
-					"name": "BasicCargo",
-					"partStats" : {
-						"cargoCapacity": 50
-					}
-				},
-				{
-					"name": "BasicGenerator",
-					"partStats" : {
-						"energyRegen": 2,
-						"energyCapacity": 10
-					}
-				}
-			]
-		}
-	]
+            shipCargo: {},
+            shipParts: [
+                {
+                    "name": "BasicEngine",
+                    "partStats": {
+                        "hull": 50,
+                        "jumpDistance": 10
+                    }
+                },
+                {
+                    "name": "BasicCargo",
+                    "partStats": {
+                        "cargoCapacity": 50
+                    }
+                },
+                {
+                    "name": "BasicGenerator",
+                    "partStats": {
+                        "energyRegen": 2,
+                        "energyCapacity": 10
+                    }
+                }
+            ]
+        }
+    ]
 };
 
 initNodes();
 require('./routes/routeManager')(app);
 // let eventMethods = require('./socket/connection/socket.connection');
-io.on('connection', function(socket) {
-    console.log('Connectin Established');
+io.on('connection', function (socket) {
+    console.log('Connectin Established', socket.id);
     // eventMethods = eventMethods(socket,idCounter, players, newPlayer);
 
-    socket.emit('connectionResponse', {'success' : true });
+    socket.emit('connectionResponse', { 'success': true });
     socket.on('login', function login(data) {
         const token = data.request.token;
- 
-        if (!playersDb.hasOwnProperty(token))
-        {
+
+        if (!playersDb.hasOwnProperty(token)) {
             playersDb[token] = newPlayer;
             idCounter++;
         }
         const player = playersDb[token];
+        connectionsId[socket.id] = player.id;
         player.ships[0].cachedShipStats = {
-            "hull" : 50,
+            "hull": 50,
             "cargoCapacity": 50,
             "jumpDistance": 10,
             "energyRegen": 2,
@@ -80,66 +81,66 @@ io.on('connection', function(socket) {
 
         players[player.id] = player;
         socket.emit('loginResponse', {
-                player : player,
-                node: nodes[player.currentNodeName],
-                nodesCoords : nodesCoords
+            player: player,
+            node: nodes[player.currentNodeName],
+            nodesCoords: nodesCoords
         });
     });
 
-    socket.on('landPlayerOnStar', function(data) {
+    socket.on('landPlayerOnStar', function (data) {
         //console.log("Landing player " + data.id + " On Star");
         validatePlayerRequest(data.player);
         const player = players[data.player.id];
         players[player.id].isLanded = true;
-		socket.leave('node-' + player.currentNodeName);
-		io.to('node-' + player.currentNodeName).emit('shipLeftNode', { playerId : player.id });	
-        socket.emit('playerLanded', { player : player });
-		delete nodes[player.currentNodeName].ships[player.id];
+        socket.leave('node-' + player.currentNodeName);
+        io.to('node-' + player.currentNodeName).emit('shipLeftNode', { playerId: player.id });
+        socket.emit('playerLanded', { player: player });
+        delete nodes[player.currentNodeName].ships[player.id];
     });
 
-    socket.on('departPlayerFromStar', function(data) {
+    socket.on('departPlayerFromStar', function (data) {
         //console.log("Departing player " + data.player.id + " From Star");
         players[data.player.id].isLanded = false;
-		io.to('node-' + data.player.currentNodeName).emit('shipEnteredNode', { ship : data.player.ships[data.player.activeShipIndex], playerId : data.player.id });		
-        socket.emit('playerDeparted', {'success' : true, player : players[data.player.id], node: nodes[data.player.currentNodeName] });	
-		nodes[data.player.currentNodeName].ships[data.player.id] = data.player.ships[data.player.activeShipIndex];
-		socket.join('node-' + data.player.currentNodeName);
+        io.to('node-' + data.player.currentNodeName).emit('shipEnteredNode', { ship: data.player.ships[data.player.activeShipIndex], playerId: data.player.id });
+        socket.emit('playerDeparted', { 'success': true, player: players[data.player.id], node: nodes[data.player.currentNodeName] });
+        nodes[data.player.currentNodeName].ships[data.player.id] = data.player.ships[data.player.activeShipIndex];
+        socket.join('node-' + data.player.currentNodeName);
     });
 
-	socket.on('playerEnteredLounge', function(data) {
+    socket.on('playerEnteredLounge', function (data) {
         //console.log("Player " + data.player.id + " Entered Lounge");
-		socket.join('lounge' + data.player.currentNodeName);
-        socket.emit('playerEnteredLounge', {'success' : true, player : players[data.id] });
+        socket.join('lounge' + data.player.currentNodeName);
+        socket.emit('playerEnteredLounge', { 'success': true, player: players[data.id] });
     });
 
-	socket.on('playerEnterMarket', function(data) {
+    socket.on('playerEnterMarket', function (data) {
         //console.log("Player " + data.player.id + " Entered Lounge");
         const player = data.player;
         validatePlayerRequest(player);
         socket.emit('playerEnteredMarket', {
-            player : players[player.id],
-            resourceSlotList : nodes[player.currentNodeName].market.resourceList
+            player: players[player.id],
+            resourceSlotList: nodes[player.currentNodeName].market.resourceList
         });
         socket.join('market' + data.player.currentNodeName);
     });
 
-	socket.on('playerLeftLounge', function(data) {
+    socket.on('playerLeftLounge', function (data) {
         //console.log("Player " + data.player.id + " Left Lounge");
-		socket.leave('lounge' + data.player.currentNodeName);
-        socket.emit('playerLeftLounge', {'success' : true, player : players[data.id] });
+        socket.leave('lounge' + data.player.currentNodeName);
+        socket.emit('playerLeftLounge', { 'success': true, player: players[data.id] });
     });
 
-	socket.on('chatSent', (data) => {
+    socket.on('chatSent', (data) => {
         //console.log("Player " + data.player.id + " Sent Chat", data);
-		//console.log("Sending message " + data.message.message + " to room '" + data.message.roomKey + data.player.currentNodeName + "' with sender Id " + data.player.id + " with name '" + data.player.firstName + "'");
-		io.to(data.message.roomKey + data.player.currentNodeName).emit('chatMessageReceived', { senderId: data.player.id, senderName: data.player.firstName, receivedMessage: data.message.message });
+        //console.log("Sending message " + data.message.message + " to room '" + data.message.roomKey + data.player.currentNodeName + "' with sender Id " + data.player.id + " with name '" + data.player.firstName + "'");
+        io.to(data.message.roomKey + data.player.currentNodeName).emit('chatMessageReceived', { senderId: data.player.id, senderName: data.player.firstName, receivedMessage: data.message.message });
     });
 
-    socket.on('playerBuyResource', function(data) {
-       console.log("Player is buying resource ", data);
-	   const player = data.player;
-	   validatePlayerRequest(player);
-		const resourceList = nodes[player.currentNodeName].market.resourceList;
+    socket.on('playerBuyResource', function (data) {
+        console.log("Player is buying resource ", data);
+        const player = data.player;
+        validatePlayerRequest(player);
+        const resourceList = nodes[player.currentNodeName].market.resourceList;
 
         if (!resourceList.hasOwnProperty(data.resource.name)) {
             console.log("This star does not contain this resource");
@@ -160,17 +161,18 @@ io.on('connection', function(socket) {
             players[data.player.id].ships[players[data.player.id].activeShipIndex].shipCargo[data.resource.name] = 0;
         }
         players[data.player.id].ships[players[data.player.id].activeShipIndex].shipCargo[data.resource.name] += data.resource.amount;
-        socket.emit('playerBoughtResource', {'success' : true, player : players[data.player.id]});
-        io.to('market'+data.player.currentNodeName).emit('updateResourceAmount', {
-            'success' : true,
+        socket.emit('playerBoughtResource', { 'success': true, player: players[data.player.id] });
+        io.to('market' + data.player.currentNodeName).emit('updateResourceAmount', {
+            'success': true,
             starName: data.player.currentNodeName,
             resourceName: data.resource.name,
-            newAmount : nodes[data.player.currentNodeName].market.resourceList[data.resource.name].amount });
+            newAmount: nodes[data.player.currentNodeName].market.resourceList[data.resource.name].amount
+        });
     });
 
-    socket.on('playerSellResource', function(data) {
+    socket.on('playerSellResource', function (data) {
         //console.log("Player is selling resource ", data);
-		const player = data.player;
+        const player = data.player;
         if (!players[data.player.id].ships[players[data.player.id].activeShipIndex].shipCargo.hasOwnProperty([data.resource.name])) {
             //console.log("Player does not have this resource");
             return;
@@ -180,89 +182,101 @@ io.on('connection', function(socket) {
             //console.log("Player Cargo: ", players[data.player.id].ships[players[data.player.id].activeShipIndex].shipCargo);
             return;
         }
-		const resourceList = nodes[player.currentNodeName].market.resourceList;
+        const resourceList = nodes[player.currentNodeName].market.resourceList;
         resourceList[data.resource.name].amount += data.resource.amount;
         players[data.player.id].credits += resourceList[data.resource.name].sellPrice;
         players[data.player.id].ships[players[data.player.id].activeShipIndex].shipCargo[data.resource.name] -= data.resource.amount;
-        socket.emit('playerSoldResource', {'success' : true, player : players[data.player.id]});
-        io.to('market'+data.player.currentNodeName).emit('updateResourceAmount', {
-            'success' : true,
+        socket.emit('playerSoldResource', { 'success': true, player: players[data.player.id] });
+        io.to('market' + data.player.currentNodeName).emit('updateResourceAmount', {
+            'success': true,
             starName: data.player.currentNodeName,
             resourceName: data.resource.name,
-            newAmount : resourceList[data.resource.name].amount
+            newAmount: resourceList[data.resource.name].amount
         });
     });
 
-    socket.on('jumpPlayerToNode', function(data) {
+    socket.on('jumpPlayerToNode', function (data) {
         //console.log("Jumping player " + data.player.id + " From Node " + data.player.currentNodeName + " To Node " + data.node.name);
         validatePlayerRequest(data.player);
         const jumpingPlayer = players[data.player.id];
-        if (jumpingPlayer.currentNodeName != data.node.name )
-        {
-			socket.leave('node-' + jumpingPlayer.currentNodeName);
-			io.to('node-' + jumpingPlayer.currentNodeName).emit('shipLeftNode', { playerId : jumpingPlayer.id });
-			delete nodes[jumpingPlayer.currentNodeName].ships[data.player.id];
-            players[jumpingPlayer.id].currentNodeName = jumpingPlayer.currentNodeName =  data.node.name;
-			io.to('node-' + jumpingPlayer.currentNodeName).emit('shipEnteredNode', { ship : jumpingPlayer.ships[jumpingPlayer.activeShipIndex], playerId : jumpingPlayer.id });	
-            socket.emit('playerJumpedToNode', {player : jumpingPlayer, node: nodes[jumpingPlayer.currentNodeName] });	
-			nodes[jumpingPlayer.currentNodeName].ships[jumpingPlayer.id] = jumpingPlayer.ships[jumpingPlayer.activeShipIndex];
-			socket.join('node-' + jumpingPlayer.currentNodeName);
+        if (jumpingPlayer.currentNodeName != data.node.name) {
+            const currentNode = nodes[jumpingPlayer.currentNodeName];
+            const destinationNode = nodes[data.node.name];
+            if (!currentNode.connectedNodes.hasOwnProperty(destinationNode.name)) {
+                socket.emit('notificationReceived', { notificationText: "Nodes are not connected!" });
+            } else {
+                let shipJumpDistance = jumpingPlayer.ships[jumpingPlayer.activeShipIndex].cachedShipStats.jumpDistance;
+                let destinationNodeJumpDistance = currentNode.connectedNodes[destinationNode.name].jumpDistance;
+                if (shipJumpDistance >= destinationNodeJumpDistance) {
+                    socket.leave('node-' + jumpingPlayer.currentNodeName);
+                    io.to('node-' + jumpingPlayer.currentNodeName).emit('shipLeftNode', { playerId: jumpingPlayer.id });
+                    delete nodes[jumpingPlayer.currentNodeName].ships[data.player.id];
+                    players[jumpingPlayer.id].currentNodeName = jumpingPlayer.currentNodeName = data.node.name;
+                    io.to('node-' + jumpingPlayer.currentNodeName).emit('shipEnteredNode', { ship: jumpingPlayer.ships[jumpingPlayer.activeShipIndex], playerId: jumpingPlayer.id });
+                    socket.emit('playerJumpedToNode', { player: jumpingPlayer, node: nodes[jumpingPlayer.currentNodeName] });
+                    nodes[jumpingPlayer.currentNodeName].ships[jumpingPlayer.id] = jumpingPlayer.ships[jumpingPlayer.activeShipIndex];
+                    socket.join('node-' + jumpingPlayer.currentNodeName);
+                } else {
+                    socket.emit('notificationReceived', { notificationText: "Engines are not strong enough to jump there!" });
+                }
+            }
         }
-        else
-        {
+        else {
             console.log("WTF player is trying to jump to the star he's at?");
         }
-        socket.emit('playerJumped', {player : players[data.player.id] });
     });
 
-    socket.on('test', function(data) {
-        console.log("Test: ", data);
+    socket.on('disconnect', function (data) {
+        const player = players[connectionsId[socket.id]];
+        const currentNode = nodes[player.currentNodeName];
+        if (currentNode.hasOwnProperty('star') && !player.isLanded) {
+            player.isLanded = true;
+        }
+        playersDb[player.id] = player;
+        delete player[player.id];
+        delete connectionsId[socket.id];
     });
 
     function validatePlayerRequest(player) {
-        if(players[player.id].token === player.token){
+        if (players[player.id].token === player.token) {
             socket.emit('invalidToken', {});
         }
     }
 });
 
 
-function logStuff()
-{
+function logStuff() {
     console.log("**************************** Players ****************************");
-    for (var playerId in players)
-    {
+    for (var playerId in players) {
         console.log("**************************** Player " + playerId + " ****************************");
         console.log(players[playerId]);
     }
 
     console.log("**************************** Stars ****************************");
-    for (var starName in nodeDb)
-    {
+    for (var starName in nodeDb) {
         console.log("**************************** Star " + starName + " ****************************");
         console.log(nodeDb[starName]);
     }
 }
 
-function initNodes()
-{
-	for (let nodeName in nodeDb) {
-		let node = nodeDb[nodeName];
+function initNodes() {
+    for (let nodeName in nodeDb) {
+        let node = nodeDb[nodeName];
         nodes[nodeName] = node;
         nodesCoords[nodeName] = {
-            name : node.name,
-            coordX : node.coordX,
-            coordY : node.coordY,
-            sprite : node.sprite,
-            connectedNodes : node.connectedNodes
+            name: node.name,
+            coordX: node.coordX,
+            coordY: node.coordY,
+            sprite: node.sprite,
+            connectedNodes: node.connectedNodes
         }
         if (node.hasOwnProperty('star')) {
             nodesCoords[nodeName].star = node.star;
         }
     }
-	//console.log(nodes);
+    //console.log(nodes);
 }
 
 server.listen(app.get('port'), function () {
-  console.log('Server listening on port ' + app.get('port'));
+    console.log('Server listening on port ' + app.get('port'));
 });
